98545caba7250fd6b55607af6994ac03caf21ee0
it21042560 <it21042560@my.sliit.lk>
2023-04-13 15:58:21 +0530

    Backend

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8040
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+   
+})
+
+const connection = mongoose.connection;
+connection.once("open", ()=>{
+    console.log("MongoDB Connection success!")
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is up and running on port ${PORT}`)
+})
